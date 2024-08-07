6be25aa51f4cc1f6467e95cff5da464cae4950d8
VivekMendhe <vivek.mendhe.022@gmail.com>
2024-08-07 12:14:03 +0530

    First Commit

---
+++ src/main/java/com/pack/vtiger/VTigerApplication.java
@@ -0,0 +1,13 @@
+package com.pack.vtiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VTigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VTigerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pack/vtiger/VTigerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.vtiger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VTigerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
