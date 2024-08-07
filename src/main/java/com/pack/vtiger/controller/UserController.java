4ea9089a1e134179eb77a831069511e483db4881
VivekMendhe <vivek.mendhe.022@gmail.com>
2024-08-07 13:52:00 +0530

    Add GetAllUser API.

--- src/main/java/com/pack/vtiger/controller/UserController.java
+++ src/main/java/com/pack/vtiger/controller/UserController.java
@@ -3,6 +3,9 @@ package com.pack.vtiger.controller;
 import com.pack.vtiger.dto.UserDTO;
 import com.pack.vtiger.service.UserService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +24,8 @@ public class UserController {
 	}
 
 	@GetMapping
-	public ResponseEntity<String> getUsers() {
-		return ResponseEntity.status(HttpStatus.OK).body("Project Run Successfully");
+	public ResponseEntity<List<UserDTO>> getUsers() {
+		List<UserDTO> users = userService.getAllUsers();
+		return ResponseEntity.status(HttpStatus.OK).body(users);
 	}
 }
--- src/main/java/com/pack/vtiger/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/pack/vtiger/serviceimpl/UserServiceImpl.java
@@ -10,48 +10,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final ModelMapper modelMapper;
-    private final UserRepository userRepository;
+	private final ModelMapper modelMapper;
+	private final UserRepository userRepository;
 
-    @Override
-    public UserDTO addUser(UserDTO userDTO) {
-        User user = this.dtoToEntity(userDTO);
-        User saveUser = this.userRepository.save(user);
-        return this.entityToDTO(saveUser);
-    }
+	@Override
+	public UserDTO addUser(UserDTO userDTO) {
+		User user = this.dtoToEntity(userDTO);
+		User saveUser = this.userRepository.save(user);
+		return this.entityToDTO(saveUser);
+	}
 
-    @Override
-    public List<UserDTO> getAllUsers() {
-        return List.of();
-    }
+	@Override
+	public List<UserDTO> getAllUsers() {
+		List<User> users = userRepository.findAll();
+		return users.stream().map(this::entityToDTO).collect(Collectors.toList());
+	}
 
-    @Override
-    public UserDTO getUserById(Integer id) {
-        return null;
-    }
+	@Override
+	public UserDTO getUserById(Integer id) {
+		return null;
+	}
 
-    @Override
-    public UserDTO updateUser(Integer id, UserDTO userDTO) {
-        return null;
-    }
+	@Override
+	public UserDTO updateUser(Integer id, UserDTO userDTO) {
+		return null;
+	}
 
-    @Override
-    public void deleteUser(Integer id) {
+	@Override
+	public void deleteUser(Integer id) {
 
-    }
+	}
 
-    // convert dto to entity
-    public User dtoToEntity(UserDTO userDTO){
-        return modelMapper.map(userDTO, User.class);
-    }
+	// convert dto to entity
+	public User dtoToEntity(UserDTO userDTO) {
+		return modelMapper.map(userDTO, User.class);
+	}
 
-    // convert entity to dto
-    public UserDTO entityToDTO(User user){
-        return modelMapper.map(user, UserDTO.class);
-    }
+	// convert entity to dto
+	public UserDTO entityToDTO(User user) {
+		return modelMapper.map(user, UserDTO.class);
+	}
 }
